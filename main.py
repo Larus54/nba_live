ca0b62cc1010b6dbd1636ad409f7ad131e0d1973
Larus54 <raimondoiannone@gmail.com>
2025-05-01 20:06:13 +0200

    Extending API

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from nba_api.live.nba.endpoints import scoreboard
 from fastapi.middleware.cors import CORSMiddleware
+from nba_api.live.nba.endpoints import scoreboard, standings
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], allow_credentials=True,
-    allow_methods=["*"], allow_headers=["*"]
+    allow_origins=["*"], allow_methods=["*"], allow_headers=["*"],
 )
 
 @app.get("/nba/today")
@@ -24,3 +23,47 @@ def get_today_games():
         }
         for g in games
     ]
+
+@app.get("/nba/standings")
+def get_standings():
+    data = standings.Standings().get_dict()["standings"]["teams"]
+    east = []
+    west = []
+
+    for team in data:
+        team_data = {
+            "team": team["teamSitesOnly"]["teamName"],
+            "wins": team["win"],
+            "losses": team["loss"],
+            "conf": team["conference"]["name"],
+            "rank": team["conference"]["rank"]
+        }
+        if team_data["conf"] == "East":
+            east.append(team_data)
+        else:
+            west.append(team_data)
+
+    # Ordina per posizione conference
+    east_sorted = sorted(east, key=lambda x: int(x["rank"]))
+    west_sorted = sorted(west, key=lambda x: int(x["rank"]))
+
+    return {
+        "east": east_sorted,
+        "west": west_sorted,
+    }
+
+@app.get("/nba/live")
+def get_live_games():
+    data = scoreboard.ScoreBoard()
+    live_games = []
+    for g in data.get_dict()["scoreboard"]["games"]:
+        if g["gameStatus"] == 2:  # 2 = IN_PROGRESS
+            live_games.append({
+                "home": g["homeTeam"]["teamName"],
+                "away": g["awayTeam"]["teamName"],
+                "homeScore": g["homeTeam"]["score"],
+                "awayScore": g["awayTeam"]["score"],
+                "clock": g["gameClock"],
+                "period": g["period"]
+            })
+    return live_games
